52559179a9c16b92d3b8d9335d6811b6ef101dc1
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2020-04-24 14:50:41 +0200

    Added failing unit test
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- test/LazyLoadingCommandTest.php
+++ test/LazyLoadingCommandTest.php
@@ -13,9 +13,13 @@ namespace LaminasTest\Cli;
 use Exception;
 use Laminas\Cli\ContainerResolver;
 use Laminas\Cli\LazyLoadingCommand;
+use LaminasTest\Cli\TestAsset\ExampleCommand;
 use LaminasTest\Cli\TestAsset\ExampleCommandWithDependencies;
 use LaminasTest\Cli\TestAsset\ExampleDependency;
 use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\NullOutput;
 
@@ -56,5 +60,31 @@ class LazyLoadingCommandTest extends TestCase
         self::expectExceptionMessage(ExampleDependency::EXCEPTION_MESSAGE);
 
         $command->run($input, $output);
+    }
+
+    public function testWillPassApplicationToCommandBeforeExecuting()
+    {
+        $container = $this->createMock(ContainerInterface::class);
+
+        $commandName = 'example:command';
+        $application = new Application();
+
+        $lazyCommand = new LazyLoadingCommand($commandName, ExampleCommand::class, $container);
+
+        $command = new ExampleCommand();
+        $container
+            ->expects($this->once())
+            ->method('get')
+            ->with(ExampleCommand::class)
+            ->willReturn($command);
+
+        $lazyCommand->setApplication($application);
+
+        $input = new ArrayInput([]);
+        $output = new NullOutput();
+
+        $lazyCommand->run($input, $output);
+
+        self::assertEquals($application, $command->getApplication());
     }
 }
--- test/TestAsset/ExampleCommand.php
+++ test/TestAsset/ExampleCommand.php
@@ -11,9 +11,15 @@ declare(strict_types=1);
 namespace LaminasTest\Cli\TestAsset;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class ExampleCommand extends Command
 {
     /** @var string */
     protected static $defaultName = 'example:command-name';
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        return 0;
+    }
 }
