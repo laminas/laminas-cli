8daf158b0b49b3681fc7695c3712943691d8a315
Matthew Weier O'Phinney <matthew@weierophinney.net>
2020-06-03 11:36:03 -0500

    qa: adds BoolParam tests
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

---
+++ test/Input/BoolParamTest.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @see       https://github.com/laminas/laminas-cli for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-cli/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-cli/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace LaminasTest\Cli\Input;
+
+use Laminas\Cli\Input\BoolParam;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+
+use function sprintf;
+
+class BoolParamTest extends TestCase
+{
+    /** @var BoolParam */
+    private $param;
+
+    public function setUp(): void
+    {
+        $this->param = new BoolParam('test');
+    }
+
+    public function testUsesValueNoneOptionMode(): void
+    {
+        $this->assertSame(InputOption::VALUE_NONE, $this->param->getOptionMode());
+    }
+
+    public function defaultValues(): iterable
+    {
+        yield 'false' => [false, 'y/N'];
+        yield 'true'  => [true, 'Y/n'];
+    }
+
+    /**
+     * @dataProvider defaultValues
+     */
+    public function testReturnsConfirmationQuestionUsingDescriptionAndDefault(
+        bool $default,
+        string $expectedDefaultString
+    ): void {
+        $description = 'This is the option description';
+        $this->param->setDefault($default);
+        $this->param->setDescription($description);
+        $expected = sprintf(
+            '<question>%s?</question> [<comment>%s</comment>]',
+            $description,
+            $expectedDefaultString
+        );
+
+        $question = $this->param->getQuestion();
+        $this->assertInstanceOf(ConfirmationQuestion::class, $question);
+        $this->assertSame($question->getQuestion(), $expected);
+    }
+}
