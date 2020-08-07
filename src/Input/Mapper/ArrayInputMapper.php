8e10afea1eefb1ccb4aaf5ece40c53f3529b16e4
Matthew Weier O'Phinney <matthew@weierophinney.net>
2020-08-07 10:46:45 -0500

    qa: more specific types and assertions fin ArrayInputMapper
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- src/Input/Mapper/ArrayInputMapper.php
+++ src/Input/Mapper/ArrayInputMapper.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
 namespace Laminas\Cli\Input\Mapper;
 
 use Symfony\Component\Console\Input\InputInterface;
+use Webmozart\Assert\Assert;
 
 use function is_array;
 use function ltrim;
@@ -18,11 +19,11 @@ use function strpos;
 
 final class ArrayInputMapper implements InputMapperInterface
 {
-    /** @psalm-var array<string, string|string[]> */
+    /** @psalm-var array<string|int, string|array<string, string>> */
     private $map;
 
     /**
-     * @psalm-param array<string, string|string[]> $map
+     * @psalm-param array<string|int, string|array<string, string>> $map
      */
     public function __construct(array $map)
     {
@@ -37,6 +38,8 @@ final class ArrayInputMapper implements InputMapperInterface
                 $params += $new;
                 continue;
             }
+
+            Assert::string($old, 'Keys in input map configuration must be strings');
 
             $params[$new] = strpos($old, '-') === 0
                 ? $input->getOption(ltrim($old, '-'))
