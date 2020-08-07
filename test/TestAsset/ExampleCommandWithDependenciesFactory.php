019e1080d7906fc81cd1618d892ed8c4488b21ed
Matthew Weier O'Phinney <matthew@weierophinney.net>
2020-08-07 10:25:53 -0500

    qa: add assertion on dependency in ExampleCommandWithDependenciesFactory
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- test/TestAsset/ExampleCommandWithDependenciesFactory.php
+++ test/TestAsset/ExampleCommandWithDependenciesFactory.php
@@ -11,12 +11,14 @@ declare(strict_types=1);
 namespace LaminasTest\Cli\TestAsset;
 
 use Psr\Container\ContainerInterface;
+use Webmozart\Assert\Assert;
 
 class ExampleCommandWithDependenciesFactory
 {
     public function __invoke(ContainerInterface $container): ExampleCommandWithDependencies
     {
         $dependency = $container->get(ExampleDependency::class);
+        Assert::isInstanceOf($dependency, ExampleDependency::class);
         return new ExampleCommandWithDependencies($dependency);
     }
 }
