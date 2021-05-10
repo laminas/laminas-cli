2f0e8bda7f14c1cdb9726ae7fe0dd7e9e04911e7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:05:01 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Command/AbstractParamAwareCommand.php
+++ src/Command/AbstractParamAwareCommand.php
@@ -1,11 +1,5 @@
 <?php // phpcs:disable WebimpressCodingStandard.Commenting.TagWithType.InvalidTypeFormat
 
-/**
- * @see       https://github.com/laminas/laminas-cli for the canonical source repository
- * @copyright https://github.com/laminas/laminas-cli/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-cli/blob/master/LICENSE.md New BSD License
- */
-
 declare(strict_types=1);
 
 namespace Laminas\Cli\Command;
